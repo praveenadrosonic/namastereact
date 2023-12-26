1aabff39be21311754cf6453bfd739f94cdd0fea
Praveen Kumar <praveen.adrosonic@gmail.com>
2023-12-26 17:57:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+
+{/* 
+<div id = "mainparent">
+<div id='parent'>
+    <div id = 'child'>
+        <div className="sib1"></div>
+        <div className="sib2"></div>
+    </div>
+</div>
+<div id='parent'>
+    <div id = 'child'>
+        <div className="sib1"></div>
+        <div className="sib2"></div>
+    </div>
+</div>
+</div>
+ */}
+
+const element1 = React.createElement("h1",{id:"parent"},
+    React.createElement("h1",{id:"child"},[React.createElement("h1",{},'sib1'),React.createElement("h1",{},'sib2')]
+));
+
+const element2 = React.createElement("h1",{id:"parent"},
+    React.createElement("h1",{id:"child"},[React.createElement("h1",{},'sib3'),React.createElement("h1",{},'sib4')]
+)); 
+
+const element = React.createElement('div',{id:'mainparent'},[element1,element2]);
+
+const roots = ReactDOM.createRoot(document.getElementById('root')); //browser realted so comes from second library 
+//
+roots.render(element); //
