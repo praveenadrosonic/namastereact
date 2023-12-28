15aba4b7ad218c41026a4a2b0b0a99b27611cf77
Praveen Kumar <praveen.adrosonic@gmail.com>
2023-12-28 15:11:27 +0530

    added jsx basic syntax

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-{/* 
+/* 
 <div id = "mainparent">
 <div id='parent'>
     <div id = 'child'>
@@ -16,7 +16,7 @@ import ReactDOM from "react-dom";
     </div>
 </div>
 </div>
-*/}
+*/
 
 const element1 = React.createElement("h1",{id:"parent"},
     React.createElement("h1",{id:"child"},[React.createElement("h1",{},'sib1'),React.createElement("h1",{},'sib2')]
@@ -31,3 +31,6 @@ const element = React.createElement('div',{id:'mainparent'},[element1,element2])
 const roots = ReactDOM.createRoot(document.getElementById('root')); //browser realted so comes from second library 
 //
 roots.render(element); //
+
+const jsxHeading = 'this is a jsx heading ...'
+roots.render(jsxHeading);
